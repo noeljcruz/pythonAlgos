1e9da6fd6c6a480a8a6372439e68ee9d2c3f5b1e
Noel Cruz <noelcruz@Noels-MacBook-Pro.local>
2025-11-10 23:14:05 +0000

    finished algo, all tests passed

--- fibonacci.py
+++ fibonacci.py
@@ -8,8 +8,13 @@ Output: n-th Fibonacci number.
 def fibonacci_number(n):
     if n <= 1:
         return n
+    
+    fibo_list = [0, 1]
 
-    return fibonacci_number(n - 1) + fibonacci_number(n - 2)
+    for i in range(2, n+1):
+        fibo_list.append(fibo_list[i-1] + fibo_list[i-2])
+
+    return fibo_list[n]
 
 
 if __name__ == '__main__':
