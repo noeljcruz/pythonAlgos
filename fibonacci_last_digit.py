c6b69970c1060496c9fd2b9a221b27dd51b31c2d
Noel Cruz <noelcruz@Noels-MacBook-Pro.local>
2025-11-11 00:06:18 +0000

    incorporated pisano period

--- fibonacci_last_digit.py
+++ fibonacci_last_digit.py
@@ -10,14 +10,24 @@ Fibonacci number.
 def fibonacci_last_digit(n):
     if n <= 1:
         return n
+    
+    fibo_list = [0, 1]
 
-    previous = 0
-    current  = 1
+    for i in range(2, 61):
+        fibo_list.append(fibo_list[i-1] + fibo_list[i-2])
+    
+    period_place = n % 60
+    return fibo_list[period_place%60]
 
-    for _ in range(n - 1):
-        previous, current = current, previous + current
+    return
 
-    return current % 10
+    # previous = 0
+    # current  = 1
+
+    # for _ in range(n - 1):
+    #     previous, current = current, previous + current
+
+    # return current % 10
 
 
 if __name__ == '__main__':
