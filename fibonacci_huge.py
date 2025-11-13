aa0c11be5260315d8f047ff5712ab4d915e417cd
Noel Cruz <noelcruz@Noels-MacBook-Pro.local>
2025-11-13 00:35:41 +0000

    completed the huge fibonacci problem, all tests passed

--- fibonacci_huge.py
+++ fibonacci_huge.py
@@ -14,6 +14,18 @@ def fibonacci_huge_naive(n, m):
     prevMod = 0
     currMod = 1
 
+    for i in range(n - 1):
+        tempMod = prevMod
+        prevMod = currMod % m
+        currMod = (currMod + tempMod) % m
+        modList.append(currMod)
+
+        if prevMod == 0 and currMod == 1:
+            index = n % (i + 1)
+            return modList[index]
+    
+    return currMod
+
     # previous = 0
     # current  = 1
 
