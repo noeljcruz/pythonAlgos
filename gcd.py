888a274b199703028e622dd8b492ac2b131b1da9
Noel Cruz <noelcruz@Noels-MacBook-Pro.local>
2025-11-13 01:45:36 +0000

    finished greatest common divisor problem

--- gcd.py
+++ gcd.py
@@ -10,14 +10,23 @@ of a and b.
 
 def gcd(a, b):
 
-    
+    if b > a:
+        a, b = b, a
     current_gcd = 1
-    for d in range(2, min(a, b) + 1):
-        if a % d == 0 and b % d == 0:
-            if d > current_gcd:
-                current_gcd = d
 
-    return current_gcd
+    while current_gcd > 0:
+        current_gcd = a % b
+        a = b
+        b = current_gcd
+
+    return a
+
+    # for d in range(2, min(a, b) + 1):
+    #     if a % d == 0 and b % d == 0:
+    #         if d > current_gcd:
+    #             current_gcd = d
+
+    # return current_gcd
 
 
 if __name__ == "__main__":
