486160aeef9e9c942e53823a1ee92963d17af8b1
Noel Cruz <noelcruz@Noels-MacBook-Pro.local>
2025-11-13 01:14:05 +0000

    added file for greatest common divisor problem

---
+++ gcd.py
@@ -0,0 +1,25 @@
+'''
+Greatest Common Divisor Problem
+Compute the greatest common divisor of two
+positive integers.
+Input: Two positive integers a
+and b.
+Output: The greatest common divisor
+of a and b.
+'''
+
+def gcd(a, b):
+
+    
+    current_gcd = 1
+    for d in range(2, min(a, b) + 1):
+        if a % d == 0 and b % d == 0:
+            if d > current_gcd:
+                current_gcd = d
+
+    return current_gcd
+
+
+if __name__ == "__main__":
+    a, b = map(int, input().split())
+    print(gcd(a, b))
