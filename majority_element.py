092f8d73626632af0eb6d7e2c610ae4a36a41cb5
Noel Cruz <noelcruz@Noels-MacBook-Pro.local>
2025-12-16 17:57:58 +0000

    majority element needs work. thinking of hashing the input list into a dic and checking if any key has a total higher than half. could also try sorting and doing some kind of search.

--- majority_element.py
+++ majority_element.py
@@ -1,3 +1,14 @@
+'''
+Majority Element Problem
+Check whether a given sequence of numbers contains
+an element that appears more than half of
+the times.
+Input: A sequence of n integers.
+Output: 1, if there is an element
+that is repeated more than n/2
+times, and 0 otherwise.
+'''
+
 def majority_element_naive(elements):
     for e in elements:
         if elements.count(e) > len(elements) / 2:
